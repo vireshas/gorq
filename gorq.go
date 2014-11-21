769102cf6eff6fcf2ea476511c51dab036d56374
Viresh <asviresh@gmail.com>
2014-11-21 15:02:38 +0530

    documenting code

--- gorq.go
+++ gorq.go
@@ -11,11 +11,21 @@ import (
 	"sync"
 )
 
+//A pool of redis connections which enqueues job in redis
 var rqRedisPool mantle.Mantle
+
+//Mutex to protect concurrent writes to rqRedisPool
 var rwMutex sync.RWMutex
 
+//Syntactic sugar for params that are passed to func
 type Hargs map[string]string
 
+/*
+ * We will be enqueuing a python job of type
+   def funcName(*args, **kwargs)
+   args: can be of type string only
+   kwargs: this is of type map[string]string
+*/
 type RQJob struct {
 	Id       string
 	funcName string
@@ -23,10 +33,15 @@ type RQJob struct {
 	kwargs   Hargs
 }
 
+//Creates a new job to enqueue
 func NewRQJob(funcName string, args []string, kwargs Hargs) *RQJob {
 	return &RQJob{Id: NewUUID(), funcName: funcName, args: args, kwargs: kwargs}
 }
 
+//Generates unique id for a job
+//TODO: check if we need a distrubted uuid generator so that
+//      jobs enqueued from multiple servers are unique
+//      Jobs in RQ expire after 442ms to I am assuming this wouldn't be an issue
 func NewUUID() string {
 	uuid, err := uuid.NewV4()
 	if err != nil {
@@ -35,6 +50,7 @@ func NewUUID() string {
 	return uuid.String()
 }
 
+//This creates a redis pool
 func InitRedisPool(whichRedis string) {
 	//protect two guys trying to read rqRedisPool at once
 	rwMutex.Lock()
@@ -44,6 +60,7 @@ func InitRedisPool(whichRedis string) {
 	}
 }
 
+//Results in RQ are pickle encoded; This method decodes the result
 func DecodeResult(result string) string {
 	//decoding encoded value
 	buf := bytes.NewBufferString(result)
@@ -56,6 +73,8 @@ func DecodeResult(result string) string {
 	return v.(string)
 }
 
+//This method encodes a string in pickle format
+//RQ expects python func that we are calling to be pickle encoded
 func (job *RQJob) EncodeJob() string {
 	//encoding stuff
 	p := &bytes.Buffer{}
@@ -66,10 +85,14 @@ func (job *RQJob) EncodeJob() string {
 	return string(p.Bytes())
 }
 
+//This was supposed to be a getter for job's id
+//I then made job Id public
+//TODO: make id private and add getters, setters
 func (job *RQJob) QueueId() string {
 	return fmt.Sprintf("rq:job:%s", job.Id)
 }
 
+//Set job related metadata to redis
 func (job *RQJob) EnqueueJob(rqJob Hargs) {
 	queueId := job.QueueId()
 	_, err := rqRedisPool.Execute("HMSET", redis.Args{queueId}.AddFlat(rqJob)...)
@@ -78,11 +101,13 @@ func (job *RQJob) EnqueueJob(rqJob Hargs) {
 	}
 }
 
+//This method encodes and then enqueues the jobs in Redis
 func (job *RQJob) Enqueue() {
 	rqJob := map[string]string{"data": job.EncodeJob()}
 	job.EnqueueJob(rqJob)
 }
 
+//This method triggers the job in RQ worker
 func (job *RQJob) Start() {
 	_, err := rqRedisPool.Execute("RPUSH", "rq:queue:default", job.Id)
 	if err != nil {
@@ -90,6 +115,7 @@ func (job *RQJob) Start() {
 	}
 }
 
+//This methid returns the result of the job
 func (job *RQJob) Result() {
 	queueId := job.QueueId()
 	fmt.Println("queueId", queueId)
